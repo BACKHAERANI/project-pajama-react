10ff07bb62b128b60da83de82db62dca1eb82342
Yerin Park <1472moment@gmail.com>
2022-02-10 17:10:55 +0900

    TopNav 구현//TopNav.jsx App.js

--- src/TopNav.jsx
+++ src/TopNav.jsx
@@ -1,5 +1,5 @@
 import { useAuth } from './Base/Context/AuthContext';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 function TopNav() {
   const [auth, , , logout] = useAuth();
@@ -8,28 +8,56 @@ function TopNav() {
   };
 
   return (
-    <div className="my-3 ">
-      <ul className="flex gap-4">
-        {!auth.isLoggedIn && (
-          <>
-            <li>
-              <MyLink to="/clothes/">의상대여</MyLink>
-            </li>
-            <li>
-              <MyLink to="/accounts/login/">Login</MyLink>
-            </li>
-            <li>
-              <MyLink to="/accounts/signup/">Signup</MyLink>
-            </li>
-          </>
-        )}
+    <div className="my-3">
+      <div className="flex justify-between justify-items-stretch">
+        <div>
+          <NavLink to="/">
+            <img src="img/logo.png" alt="logo-img" />
+          </NavLink>
+        </div>
 
-        {auth.isLoggedIn && (
-          <>
-            <button onClick={handleLogout}>로그아웃</button>
-          </>
-        )}
-      </ul>
+        <div>
+          <div className="flex justify-end mt-2 md:flex-row md:mt-0 md:mx-1">
+            {!auth.isLoggedIn && (
+              <>
+                <MyLink to="/accounts/login/">로그인</MyLink>
+
+                <MyLink to="/accounts/login/">마이페이지</MyLink>
+
+                <MyLink to="/accounts/login/">장바구니</MyLink>
+              </>
+            )}
+
+            {auth.isLoggedIn && (
+              <>
+                <button
+                  className="pb-1 text-gray-500 text-xs flex flex-col mt-2 md:flex-row md:mt-0 md:mx-1
+                          hover:text-purple-300 "
+                  onClick={handleLogout}
+                >
+                  로그아웃
+                </button>
+                <MyLink to="/accounts//">마이페이지</MyLink>
+
+                <MyLink to="/accounts/login/">장바구니</MyLink>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="flex">
+        <div className="text-align: right-1 mt-6 ml-12 ">
+          <Link to="/clothes/" className="text-gray-800">
+            의상대여
+          </Link>
+        </div>
+        <div className="text-align: right-1 mt-6 ml-12 ">
+          <Link to="/clothes/" className="text-gray-800">
+            가나다
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
@@ -38,8 +66,8 @@ function MyLink({ to, children }) {
   return (
     <Link
       to={to}
-      className="pb-1 text-gray-500 
-      hover:text-purple-300 hover:border-purple-300 border-b-2"
+      className="flex-justify-between pb-1 text-gray-500 text-xs flex flex-col mt-2 md:flex-row md:mt-0 md:mx-1
+      hover:text-purple-300 "
     >
       {children}
     </Link>
