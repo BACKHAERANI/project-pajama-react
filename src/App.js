5984ecf67e5ebd6f16da35b2f1e012e5ba3136e4
Yerin Park <1472moment@gmail.com>
2022-02-14 19:26:06 +0900

    QnA 앱 연결//App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ import PageProfileDetail from './Pages/mypage/PageProfileDetail';
 import PageProfileForm from './Pages/mypage/PageProfileForm';
 import PageNoticeDetail from './Pages/notice/PageNoticeDetail';
 import PageNoticeForm from './Pages/notice/PageNoticeForm';
+import PageQnaIndex from './Pages/qna/PageQnaIndex';
+import PageQnaForm from './Pages/qna/PageQnaForm';
+import PageQnaDetail from './Pages/qna/PageQnaDetail';
 
 function App() {
   return (
@@ -23,17 +26,25 @@ function App() {
         <Route path="/" element={<PageMain />} />
         <Route path="/accounts/login/" element={<PageLogin />} />
         <Route path="/accounts/signup/" element={<PageSignup />} />
+
         <Route path="/clothes/" element={<PageClothesIndex />} />
         <Route path="/clothes/:clothesId/" element={<PageClothesDetail />} />
         <Route path="/clothes/new/" element={<PageClothesForm />} />
         <Route path="/clothes/:clothesId/edit/" element={<PageClothesForm />} />
+
         <Route path="/notice/" element={<PageNoticeList />} />
         <Route path="/notice/:notice_num/" element={<PageNoticeDetail />} />
         <Route path="/notice/new/" element={<PageNoticeForm />} />
         <Route path="/notice/:notice_num/edit" element={<PageNoticeForm />} />
+
         <Route path="/mypage/profile/check/" element={<PageProfileCheck />} />
         <Route path="/mypage/profile/detail/" element={<PageProfileDetail />} />
         <Route path="/mypage/profile/edit/" element={<PageProfileForm />} />
+
+        <Route path="/qna/" element={<PageQnaIndex />} />
+        <Route path="/qna/:qna_num/" element={<PageQnaDetail />} />
+        <Route path="/qna/new/" element={<PageQnaForm />} />
+        <Route path="/qna/:qna_num/edit/" element={<PageQnaForm />} />
       </Routes>
 
       <hr className="mt-5" />
