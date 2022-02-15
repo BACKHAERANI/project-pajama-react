f82af52ca5f0730eacc902aa62bb30c96d05b2e1
Yerin Park <1472moment@gmail.com>
2022-02-15 19:39:29 +0900

    SideNav 구현//src/SideNav.jsx

---
+++ src/SideNav.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+function SideNav() {
+  return (
+    <div className="flex flex-col pr-20 ">
+      <div className="text-align: text-xl right-1 mt-8 mb-3 ml-5 mr-12 ">
+        마이페이지
+      </div>
+      <MyLink to="/clothes/">대여 내역</MyLink>
+      <MyLink to="/review/">대여 후기</MyLink>
+      <MyLink to="/profile/">개인정보수정</MyLink>
+      <MyLink to="/qna/">1:1 문의</MyLink>
+    </div>
+  );
+}
+
+function MyLink({ to, children }) {
+  return (
+    <Link to={to} className="text-align: pl-2 py-3 box-border border">
+      {children}
+    </Link>
+  );
+}
+
+export default SideNav;
