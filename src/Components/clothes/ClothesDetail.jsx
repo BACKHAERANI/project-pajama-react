819e81b0e033efe48436d37562576e4b984423d8
Hyeonil Na <sjrnfl528@gmail.com>
2022-02-18 15:14:41 +0900

    clothes 작성자 추가, 본인글만 수정/삭제 가능

--- src/Components/clothes/ClothesDetail.jsx
+++ src/Components/clothes/ClothesDetail.jsx
@@ -4,9 +4,11 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import LoadingIndicator from 'Components/LoadingIndicator';
 import PageReviewIndex from 'Pages/review/PageReviewIndex';
+import { useAuth } from 'Base/Context/AuthContext';
 
 function ClothesDetail({ clothes_num }) {
   const navigate = useNavigate();
+  const [auth] = useAuth();
 
   const [{ data: clothes, loading, error }, refetch] = useApiAxios(
     {
@@ -48,11 +50,15 @@ function ClothesDetail({ clothes_num }) {
 
       {clothes && (
         <>
-          <h3 className="text-2xl my-5">{clothes.title}</h3>
-          <h4 className="text-xl my-5">[카테고리:{clothes.category}]</h4>
-          <h5 className="text-l my-5">
-            [지역:{clothes.region}] [가격:{clothes.price}원]
-          </h5>
+          <div className="grid">
+            <h3 className="text-2xl my-5">{clothes.title}</h3>
+            <h4 className="text-xl my-5">[카테고리:{clothes.category}]</h4>
+            <h5 className="text-l my-5">
+              [작성자:{clothes?.user_id?.user_nickname}] [지역:{clothes.region}]
+              [가격:
+              {clothes.price}원]
+            </h5>
+          </div>
           <div>
             {clothes.content.split(/[\r\n]+/).map((line, index) => (
               <p className="my-3" key={index}>
@@ -60,21 +66,23 @@ function ClothesDetail({ clothes_num }) {
               </p>
             ))}
           </div>
-          {clothes.img1 && (
-            <img src={clothes.img1} alt={clothes.title} className="rounded" />
-          )}
-          {clothes.img2 && (
-            <img src={clothes.img2} alt={clothes.title} className="rounded" />
-          )}
-          {clothes.img3 && (
-            <img src={clothes.img3} alt={clothes.title} className="rounded" />
-          )}
-          {clothes.img4 && (
-            <img src={clothes.img4} alt={clothes.title} className="rounded" />
-          )}
-          {clothes.img5 && (
-            <img src={clothes.img5} alt={clothes.title} className="rounded" />
-          )}
+          <div className="grid grid-cols-2 gap-4 my-20">
+            {clothes.img1 && (
+              <img src={clothes.img1} alt={clothes.title} className="rounded" />
+            )}
+            {clothes.img2 && (
+              <img src={clothes.img2} alt={clothes.title} className="rounded" />
+            )}
+            {clothes.img3 && (
+              <img src={clothes.img3} alt={clothes.title} className="rounded" />
+            )}
+            {clothes.img4 && (
+              <img src={clothes.img4} alt={clothes.title} className="rounded" />
+            )}
+            {clothes.img5 && (
+              <img src={clothes.img5} alt={clothes.title} className="rounded" />
+            )}
+          </div>
         </>
       )}
       <hr className="my-3" />
@@ -83,22 +91,37 @@ function ClothesDetail({ clothes_num }) {
       </h5>
       <hr className="my-3" />
       <div className="flex flex-row-reverse gap-4 mt-3 mb-10">
-        <Link to="/clothes/" className="hover:text-red-400">
+        <Link to="/clothes/" className="ml-4 hover:text-purple-400">
           목록
         </Link>
-        <Link
-          to={`/clothes/${clothes_num}/edit/`}
-          className="hover:text-red-400"
-        >
-          수정
-        </Link>
-        <button
-          disabled={deleteLoading}
-          onClick={handleDelete}
-          className="hover:text-red-400"
-        >
-          삭제
-        </button>
+        {clothes?.user_id?.user_id === auth.user_id && !auth.is_superuser && (
+          <>
+            <Link
+              to={`/clothes/${clothes_num}/edit/`}
+              className="ml-4 hover:text-purple-400"
+            >
+              수정
+            </Link>
+            <button
+              disabled={deleteLoading}
+              onClick={handleDelete}
+              className="hover:text-red-400"
+            >
+              삭제
+            </button>
+          </>
+        )}
+        {auth.is_superuser && (
+          <>
+            <button
+              disabled={deleteLoading}
+              onClick={handleDelete}
+              className="hover:text-red-400"
+            >
+              삭제
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
