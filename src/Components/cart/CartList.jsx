8ef15b9016ad356c940e9cc68bab74ea6e83ddf8
back hae ran <idg9833@gmail.com>
2022-02-18 18:13:14 +0900

    cart 구현 // cartnumber index로 받도록 변경, 체크박스 형태 구현

--- src/Components/cart/CartList.jsx
+++ src/Components/cart/CartList.jsx
@@ -1,6 +1,7 @@
 import { useApiAxios } from 'Base/api/base';
 import { useAuth } from 'Base/Context/AuthContext';
-import { useEffect } from 'react';
+import DebugStates from 'DebugStates';
+import { useEffect, useState } from 'react';
 import CartSummary from './CartSummary';
 
 function CartList() {
@@ -9,24 +10,45 @@ function CartList() {
     { url: '/cart/api/cart/', method: 'GET' },
     { manual: true },
   );
+  const [shoppingList, setShoppingList] = useState([]);
 
   useEffect(() => {
     refetch();
   }, []);
 
+  const handleSingleCheck = (e) => {
+    const value = e.target.value;
+    console.log(value);
+  };
+
   return (
     <div>
       <h1 className="text-lx">장바구니</h1>
 
+      <hr />
+
       {cartList &&
         cartList
           .filter((cart) => cart.user_id.user_id === auth.user_id)
-          .map((cart) => (
+          .map((cart, index) => (
             <div>
+              <input
+                type="checkbox"
+                name="cart_num"
+                value={cart.cart_num}
+                onChange={handleSingleCheck}
+                // checked={bChecked}
+                // onChange={(e) => checkHandler(e)}
+              />
+              <div className="text-lg"> {index + 1} </div>
               <CartSummary cart={cart} key={cart.cart_num} />
               <hr />
             </div>
           ))}
+
+      <button>결제</button>
+
+      <DebugStates cartList={cartList} />
     </div>
   );
 }
--- src/Components/cart/CartSummary.jsx
+++ src/Components/cart/CartSummary.jsx
@@ -2,12 +2,12 @@ import { Link, useParams } from 'react-router-dom';
 
 function CartSummary({ cart }) {
   return (
-    <div>
-      <div>{cart.cart_num}</div>
+    <div className="flex">
       <Link to={`/clothes/${cart?.clothes_num.clothes_num}/`}>
-        <img className="rounded-full w-40" src={cart?.clothes_num?.img1} />
         <div>{cart?.clothes_num?.title}</div>
+        <img className="rounded-full w-40" src={cart?.clothes_num?.img1} />
       </Link>
+      <div>{cart?.clothes_num?.price}원</div>
       <div>{cart.rental_date}</div>
       <div>{cart.return_date}</div>
     </div>
