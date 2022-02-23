9806647cb53deae02ff54d70bf9ed225b0720364
back hae ran <idg9833@gmail.com>
2022-02-23 15:25:19 +0900

    cart 구현 // 선택된 체크박스 삭제

--- src/Pages/cart/PageCartList.jsx
+++ src/Pages/cart/PageCartList.jsx
@@ -1,6 +1,8 @@
 import CartList from 'Components/cart/CartList';
+import { useParams } from 'react-router-dom';
 
 function PageCartList() {
+  const { cart_num } = useParams();
   return (
     <div>
       <CartList />
