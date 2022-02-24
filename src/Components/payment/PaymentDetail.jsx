cc92e210cd520e1c8168a7f6ca36c60c737cb7fa
back hae ran <idg9833@gmail.com>
2022-02-24 11:01:56 +0900

    payment 구현 // 결제 후 보여줄 PaymentDetail 화면

--- src/Components/payment/PaymentDetail.jsx
+++ src/Components/payment/PaymentDetail.jsx
@@ -2,13 +2,14 @@ import { useApiAxios } from 'Base/api/base';
 import { useEffect } from 'react';
 
 function PaymentDetail({ payment_num }) {
-  const [{ data: payment, loading, error }, refetch] = useApiAxios(
-    {
-      url: `/payment/api/payment/${payment_num}/`,
-      method: 'GET',
-    },
-    { manual: true },
-  );
+  const [{ data: payment, loading: payLoading, error: payError }, refetch] =
+    useApiAxios(
+      {
+        url: `/payment/api/payment/${payment_num}/`,
+        method: 'GET',
+      },
+      { manual: true },
+    );
 
   useEffect(() => {
     refetch();
@@ -17,14 +18,13 @@ function PaymentDetail({ payment_num }) {
   return (
     <div>
       <h2>예약이 완료되었습니다.</h2>
-      <>
-        {payment && (
-          <div>
-            <span>총 결제금액 : {payment.total_price}</span>
-            <span>결제 방법 : {payment.payment_method}</span>
-          </div>
-        )}
-      </>
+      {payment && (
+        <div>
+          <span>총 결제금액 : {payment.total_price}</span>
+          <span>결제 방법 : {payment.payment_method}</span>
+        </div>
+      )}
+
       <div>re-danse를 이용해주셔서 감사합니다. 우리 또, 만나요.</div>
     </div>
   );
