5ca97ffbba6d2260f6adb3817d87583712a18b60
back hae ran <idg9833@gmail.com>
2022-02-28 16:44:03 +0900

    review 구현 // Rentalreview에서 리뷰 삭제 구현

--- src/Components/mypage/Rentalreview.jsx
+++ src/Components/mypage/Rentalreview.jsx
@@ -1,12 +1,15 @@
 import { useApiAxios } from 'Base/api/base';
 import { useAuth } from 'Base/Context/AuthContext';
-import ReviewSummary from 'Components/review/ReviewSummary';
+import Rating from 'Pages/review/Rating';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SideNav from 'SideNav';
 
-function Rentalreview() {
+function Rentalreview({ score }) {
+  const Navigate = useNavigate();
   const [auth] = useAuth();
   const [currentItems, setCurrentItems] = useState(null);
+  const [reload, setReload] = useState(false);
   const [{ data, loading, error }, getReview] = useApiAxios(
     { url: '/review/api/review_detail/', method: 'GET' },
     { manual: true },
@@ -16,7 +19,31 @@ function Rentalreview() {
     getReview().then((response) => {
       setCurrentItems(response?.data.results);
     });
-  }, []);
+  }, [reload]);
+
+  //리뷰 삭제
+
+  const [{ loading: deleteLoading, error: deleteError }, deletereview] =
+    useApiAxios(
+      {
+        url: `/review/api/review_detail/${data?.results?.payment_detail_num}`,
+        method: 'DELETE',
+      },
+      { manual: true },
+    );
+
+  const handleDelete = (e) => {
+    const { value: payment_detail_num } = e.target;
+
+    if (window.confirm('삭제하시겠습니까?')) {
+      deletereview({
+        url: `/review/api/review_detail/${payment_detail_num}`,
+        method: 'DELETE',
+      }).then(() => {
+        setReload((prevState) => !prevState);
+      });
+    }
+  };
 
   return (
     <div className="flex">
@@ -30,9 +57,13 @@ function Rentalreview() {
               <div>{review.title}</div>
               <div>{review.content}</div>
               <div>{review.score}</div>
+              <Rating score={review.score} />
               <img className="h-20 w-20" src={review.clothes_num.img1} />
               <div>{review.clothes_num.title}</div>
               <div className="ml-10">{review.clothes_num.price}원</div>
+              <button onClick={handleDelete} value={review.payment_detail_num}>
+                삭제
+              </button>
             </div>
           );
         })}
