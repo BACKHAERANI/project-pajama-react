f2977502f195f0a96bb088290bdf842dc1e2fbf0
Yerin Park <1472moment@gmail.com>
2022-03-02 02:27:32 +0900

    PageQnaForm 수정 // tailwind CSS 스타일  수정

--- src/Pages/qna/PageQnaForm.jsx
+++ src/Pages/qna/PageQnaForm.jsx
@@ -1,15 +1,25 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import QnaForm from 'Components/qna/QnaForm';
+import SideNav from 'SideNav';
 
 function PageQnaForm() {
   const navigate = useNavigate();
   const { qna_num } = useParams();
 
   return (
-    <QnaForm
-      qna_num={qna_num}
-      handleDidSave={(savedQna) => navigate(`/qna/${savedQna.qna_num}/`)}
-    />
+    <div className="grid grid-cols-5">
+      <div>
+        <SideNav />
+      </div>
+
+      <div className="grid col-span-4 auto-rows-max">
+        <h1 className=" pt-10 pb-5 text-xl font-bold">1:1 문의</h1>
+        <QnaForm
+          qna_num={qna_num}
+          handleDidSave={(savedQna) => navigate(`/qna/${savedQna.qna_num}/`)}
+        />
+      </div>
+    </div>
   );
 }
 
