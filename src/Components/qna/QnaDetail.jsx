303031d4a262b011c125980a6e606ef2745948f4
Yerin Park <1472moment@gmail.com>
2022-03-02 15:53:17 +0900

    QnaDetail CSS 1차 구현 // QnaDetail.jsx

--- src/Components/qna/QnaDetail.jsx
+++ src/Components/qna/QnaDetail.jsx
@@ -2,8 +2,10 @@ import { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useApiAxios } from 'Base/api/base';
 import LoadingIndicator from 'Components/LoadingIndicator';
+import { useAuth } from 'Base/Context/AuthContext';
 
 function QnaDetail({ qna_num }) {
+  const [auth] = useAuth();
   const navigate = useNavigate();
   const [{ data: qna, loading, error }, refetch] = useApiAxios(
     { url: `/qna/api/qna/${qna_num}/`, method: 'GET' },
@@ -36,50 +38,64 @@ function QnaDetail({ qna_num }) {
         `삭제 요청 중 에러 발생(${deleteError.response.status} ${deleteError.response.statusText})`}
       {qna && (
         <div>
-          <hr className=" border-t border-gray-400" />
+          <hr className=" border-t border-gray-300 " />
+          <div className="grid grid-cols-6   border border-gray-300 ">
+            <div className="bg-gray-200">
+              <label className=" mt-4 flex justify-center ">제목</label>
+            </div>
+            <div className="col-span-3">
+              <p className=" col-start-3 my-4 ml-4 mr-0 w-10/12">{qna.title}</p>
+            </div>
 
-          <div className="col-span-4">
-            <label className="bg-gray-100 font-medium pr-20 ">제목</label>
-            <p className=" col-start-3 my-4 ml-4 mr-0 p-1 w-10/12">
-              {qna.title}
-            </p>
+            <div className="bg-gray-200">
+              <label className=" mt-4 flex justify-center ">작성일</label>
+            </div>
+            <div>
+              <p className=" col-start-3 my-4 ml-4 mr-0  w-10/12">
+                {qna.registration_date.slice(0, 10)}
+              </p>
+            </div>
           </div>
-          <div className="col-span-4">
-            <label className="bg-gray-100 font-medium pr-20 ">작성일</label>
-            <p className=" col-start-3 my-4 ml-4 mr-0 p-1 w-10/12">
-              {qna.registration_date.slice(0, 10)}
-            </p>
+          <div className="border border-t-0 border-gray-300">
+            <div className="col-span-6 pl-8 pt-6">
+              {qna.content.split(/[\r\n]/).map((line, index) => (
+                <p key={index}>{line}</p>
+              ))}
+            </div>
+            <div className="col-span-6 pl-8 py-6 max-h-full max-w-full">
+              {qna.img && (
+                <img
+                  className="max-w-3xl max-h-full"
+                  src={qna.img}
+                  alt={qna.title}
+                />
+              )}
+            </div>
+            <div className="bg-gray-200">{qna.answer}</div>
           </div>
-
-          {qna.img && (
-            <img
-              className="max-w-3xl max-h-full"
-              src={qna.img}
-              alt={qna.title}
-            />
-          )}
-          <div>
-            {qna.content.split(/[\r\n]/).map((line, index) => (
-              <p key={index}>{line}</p>
-            ))}
-          </div>
-          <div className="bg-gray-200">{qna.answer}</div>
         </div>
       )}
-      <div>
-        <div className="p-4 pt-7 pr-1 text-sm text-right inline-block align-middle">
-          <button className="w-24 h-8 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600">
-            <Link to="/qna/">목록</Link>
-          </button>
-        </div>
-      </div>
-      <div className="p-4 pt-7 pr-1 text-sm text-right inline-block align-middle">
+
+      <div className=" flex justify-end p-1 mt-3  text-sm align-middle">
         <button
-          className="w-24 h-8 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600"
+          className="w-24 h-8 ml-2  bg-white rounded-sm text-gray-500 border border-gray-300 "
           disabled={deleteLoading}
           onClick={handleDlete}
         >
           삭제
+        </button>
+        {auth.is_superuser && (
+          <>
+            <button
+              className="w-24 h-8 ml-2 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600"
+              onClick={() => navigate(`/qna/${qna_num}/edit/`)}
+            >
+              답변
+            </button>
+          </>
+        )}
+        <button className="w-24 h-8 ml-2 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600">
+          <Link to="/qna/">목록</Link>
         </button>
       </div>
     </div>
