3aa28c455a3d946c99ad14575e5211fac01d2d98
back hae ran <idg9833@gmail.com>
2022-03-02 16:28:13 +0900

    notice 구현 // 공지사항 디테일 페이지 css 수정

--- src/Components/notice/NoticeDetail.jsx
+++ src/Components/notice/NoticeDetail.jsx
@@ -40,13 +40,34 @@ function NoticeDetail({ notice_num }) {
         `로딩 중 에러가 발생했습니다. (${error.response.status} ${error.response.statusText})`}
       {notice && (
         <>
-          <h3 className="text-2xl my-5">{notice.title}</h3>
-          <p>by {notice?.user_id?.user_nickname}</p>
-          {notice.img1 && <img src={notice.img1} alt={notice.title} />}
-          {notice.img2 && <img src={notice.img2} alt={notice.title} />}
-          {notice.img3 && <img src={notice.img3} alt={notice.title} />}
-          {notice.img4 && <img src={notice.img4} alt={notice.title} />}
-          {notice.img5 && <img src={notice.img5} alt={notice.title} />}
+          <hr className=" border-t border-gray-300" />
+          <div className="grid grid-cols-6 border border-gray-300">
+            <div className="bg-gray-200">
+              <label className=" mt-4 flex justify-center ">제목</label>
+            </div>
+            <div className="col-span-3">
+              <p className=" col-start-3 my-4 ml-4 mr-0 w-10/12">
+                {notice.title}
+              </p>
+            </div>
+            <div className="bg-gray-200">
+              <label className="mt-4 flex justify-center">작성자</label>
+            </div>
+            <div>
+              <p className="col-start-3 my-4 ml-4 mr-0 w-10/12">
+                {notice?.user_id?.user_nickname}
+              </p>
+            </div>
+          </div>
+          <div className="col-span-6 pl-8 py-6 max-h-full max-w-full">
+            {notice.img1 && (
+              <img
+                src={notice.img1}
+                alt={notice.title}
+                className="max-w-3xl max-h-full"
+              />
+            )}
+          </div>
           <div>
             {notice.content.split(/[\r\n]+/).map((line, index) => (
               <p className="my-3" key={index}>
@@ -54,31 +75,78 @@ function NoticeDetail({ notice_num }) {
               </p>
             ))}
           </div>
+          <div className="col-span-6 pl-8 py-6 max-h-full max-w-full">
+            {notice.img2 && (
+              <img
+                src={notice.img2}
+                alt={notice.title}
+                className="max-w-3xl max-h-full"
+              />
+            )}
+          </div>
+          <div className="col-span-6 pl-8 py-6 max-h-full max-w-full">
+            {notice.img3 && (
+              <img
+                src={notice.img3}
+                alt={notice.title}
+                className="max-w-3xl max-h-full"
+              />
+            )}
+          </div>
+          <div className="col-span-6 pl-8 py-6 max-h-full max-w-full">
+            {notice.img4 && (
+              <img
+                src={notice.img4}
+                alt={notice.title}
+                className="max-w-3xl max-h-full"
+              />
+            )}
+          </div>
+          <div className="col-span-6 pl-8 py-6 max-h-full max-w-full">
+            {notice.img5 && (
+              <img
+                src={notice.img5}
+                alt={notice.title}
+                className="max-w-3xl max-h-full"
+              />
+            )}
+          </div>
         </>
       )}
       <hr className="my-3" />
-      <div className="flex">
+      <div className="flex justify-end">
         {auth.is_superuser && (
-          <>
-            <button
-              disabled={deleteLoading}
-              onClick={handleDelete}
-              className="hover:text-red-400"
-            >
-              삭제
-            </button>
-            <Link
-              to={`/notice/${notice_num}/edit/`}
-              className="ml-4 hover:text-blue-400"
-            >
-              수정
-            </Link>
-          </>
+          <div>
+            <div className="p-4 pt-7 pr-1 text-sm text-right inline-block align-middle">
+              <button
+                disabled={deleteLoading}
+                onClick={handleDelete}
+                className="w-24 h-8 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600"
+              >
+                삭제
+              </button>
+            </div>
+            <div className="pt-3 ml-2 pr-1 text-sm text-right inline-block align-middle">
+              <Link
+                className="w-24 h-8 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600"
+                type="button"
+                to={`/notice/${notice_num}/edit/`}
+              >
+                <h1 className="text-center mt-1.5 text-sm">수정</h1>
+              </Link>
+            </div>
+          </div>
         )}
 
-        <Link to="/notice/" className="ml-4 hover:text-purple-400">
-          목록
-        </Link>
+        <div className="mt-7 ml-2 pr-1 inline-block align-middle flex justify-end">
+          <Link
+            className="w-24 h-8 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600"
+            type="button"
+            to="/notice/"
+          >
+            <h1 className="text-center mt-1.5 text-sm">목록</h1>
+          </Link>
+        </div>
       </div>
     </div>
   );
