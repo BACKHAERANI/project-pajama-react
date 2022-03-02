3885b6f920f468b485c549aabb01b5f38fee4752
back hae ran <idg9833@gmail.com>
2022-03-03 05:45:01 +0900

    payment 구현 // payment css 1차구현

--- src/Components/payment/Payment.jsx
+++ src/Components/payment/Payment.jsx
@@ -106,89 +106,128 @@ function Payment() {
 
   return (
     <div>
-      <div>
-        <DebugStates fieldValues={fieldValues} />
-        {saveLoading && <LoadingIndicator>저장하고 있어요.</LoadingIndicator>}
-        {saveError &&
-          `저장 중 에러가 발생했습니다.(${saveError.response.status} ${saveError.response.statusText})`}
-        <h2 className="text-lg">결제</h2>
-        {state?.map((check) => {
-          return (
-            <div className="flex" key={check.cart_num}>
-              <img className="h-20 w-20" src={check.clothes_num.img1} />
-              <div>{check.clothes_num.title}</div>
-              <div className="ml-10">{check.clothes_num.price}원</div>
-              <div className="ml-10">
-                {check.rental_date}~{check.return_date}
-              </div>
-            </div>
-          );
-        })}
-        <hr />
-        <div>
-          <span>----대여회원정보----</span>
+      {saveLoading && <LoadingIndicator>저장하고 있어요.</LoadingIndicator>}
+      {saveError &&
+        `저장 중 에러가 발생했습니다.(${saveError.response.status} ${saveError.response.statusText})`}
 
-          {filterstate.map((check) => {
+      <div className="grid grid-cols-8 m-10 mb-0  ">
+        <div className="">
+          <h1 className="text-xl font-bold m-auto">결제💰</h1>
+        </div>
+        <hr className="col-start-2 col-span-7 pt-5 border-t-2 border-gray-400" />
+        <div className="col-start-3 col-span-2 m-auto">상품정보</div>
+        <div className="col-start-6 mr-2 m-auto">가격</div>
+        <div className="col-start-7 col-span-2 m-auto">대여기간</div>
+      </div>
+
+      <div className="grid grid-cols-8 m-10 mt-2">
+        <div className=""></div>
+
+        <div className="col-span-7">
+          {state?.map((check) => {
             return (
-              <div key={check.cart_num}>
-                <span>회원이름:</span>
-                <div>{check.user_id?.username}</div>
-                <span>회원전화번호:</span>
-                <div>{check.user_id?.user_tel}</div>
+              <div
+                className="grid grid-cols-8 my-3 pt-3 border-t border-gray-400"
+                key={check.cart_num}
+              >
+                <div>
+                  <img
+                    className="h-20 w-20 object-cover shrink-0 col-start-1 col-span-2 m-auto"
+                    src={check.clothes_num.img1}
+                  />
+                </div>
+                <div className="col-start-3 col-span-2 m-auto">
+                  {check.clothes_num.title}
+                </div>
+                <div className="col-start-6 m-auto">
+                  {check.clothes_num.price}원
+                </div>
+                <div className="col-start-7 col-span-2 m-auto">
+                  {check.rental_date}~{check.return_date}
+                </div>
               </div>
             );
           })}
-        </div>
-      </div>
+          <hr />
 
-      <hr />
-      <form onSubmit={handleSave}>
-        <div>
           <div>
-            <select name="return_method">
-              <option>반납방법을 선택해주세요!</option>
-              <option>직접반납</option>
-              <option>택배반납(+2500원 추가)</option>
-            </select>
+            <span>----대여회원정보----</span>
+
+            {filterstate.map((check) => {
+              return (
+                <div key={check.cart_num}>
+                  <span>회원이름:</span>
+                  <div>{check.user_id?.username}</div>
+                  <span>회원전화번호:</span>
+                  <div>{check.user_id?.user_tel}</div>
+                </div>
+              );
+            })}
           </div>
+        </div>
+
+        <hr />
+        <form onSubmit={handleSave}>
           <div>
-            <select
-              name="payment_method"
-              onChange={handleFieldChange}
-              value={fieldValues.payment_method}
+            <div>
+              <select name="return_method">
+                <option>반납방법을 선택해주세요!</option>
+                <option>직접반납</option>
+                <option>택배반납(+2500원 추가)</option>
+              </select>
+            </div>
+            <div>
+              <select
+                name="payment_method"
+                onChange={handleFieldChange}
+                value={fieldValues.payment_method}
+              >
+                <option value="selected">결제방법을 선택해주세요!</option>
+                <option value="카드">카드</option>
+                <option value="현금">만나서 현금결제</option>
+              </select>
+            </div>
+          </div>
+          <hr />
+          <div>
+            <h2 className="text-lg">----총결제정보----</h2>
+            <div>
+              <span>대여의상 수:</span>
+              <input
+                className="outline-none"
+                name="total_amount"
+                onChange={handleFieldChange}
+                value={amount}
+              />
+            </div>
+
+            <div>
+              <span>총결제금액:</span>
+              <input
+                className="outline-none"
+                name="total_price"
+                onChange={handleFieldChange}
+                value={totalPrice}
+              />
+            </div>
+          </div>
+
+          <hr className="border-t border-gray-400" />
+
+          <button className="w-24 h-8 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600">
+            결제
+          </button>
+          <div className=" mt-2 text-sm text-right inline-block align-middle">
+            <Link
+              className="w-24 h-8 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600"
+              type="button"
+              to={'/cart/'}
             >
-              <option value="selected">결제방법을 선택해주세요!</option>
-              <option value="카드">카드</option>
-              <option value="현금">만나서 현금결제</option>
-            </select>
+              <h1 className="text-center mt-1.5 text-sm">취소</h1>
+            </Link>
           </div>
-        </div>
-        <hr />
-        <div>
-          <h2 className="text-lg">----총결제정보----</h2>
-          <div>
-            <span>대여의상 수:</span>
-            <input
-              name="total_amount"
-              onChange={handleFieldChange}
-              value={amount}
-            />
-          </div>
-
-          <div>
-            <span>총결제금액:</span>
-            <input
-              name="total_price"
-              onChange={handleFieldChange}
-              value={totalPrice}
-            />
-          </div>
-        </div>
-        <hr />
-        <button className="text-lg mr-10 hover:bg-purple-300">결제</button>
-        <Link to={'/cart/'}>취소 </Link>
-      </form>
-      <DebugStates state={state} />
+        </form>
+      </div>
     </div>
   );
 }
