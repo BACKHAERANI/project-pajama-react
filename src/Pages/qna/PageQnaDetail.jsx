8ef778082806702ab6e0e67583b1b97c1e26b5dd
Yerin Park <1472moment@gmail.com>
2022-03-02 18:58:21 +0900

    PageQnaDetail, PageQnaForm 수정 // admin page 분리

--- src/Pages/qna/PageQnaDetail.jsx
+++ src/Pages/qna/PageQnaDetail.jsx
@@ -1,22 +1,40 @@
 import { useParams } from 'react-router-dom';
 import QnaDetail from 'Components/qna/QnaDetail';
 import SideNav from 'SideNav';
+import { useAuth } from 'Base/Context/AuthContext';
+import SideNavAdmin from 'SideNavAdmin';
 
 function PageQnaDetail() {
   const { qna_num } = useParams();
+  const [auth] = useAuth();
 
-  return (
-    <div className="grid grid-cols-5">
-      <div>
-        <SideNav />
-      </div>
+  if (auth.isLoggedIn && auth.is_superuser) {
+    return (
+      <div className="grid grid-cols-5">
+        <div>
+          <SideNavAdmin />
+        </div>
 
-      <div className="grid col-span-4 auto-rows-max">
-        <h1 className=" pt-10 pb-5 text-xl font-bold">1:1 문의</h1>
-        <QnaDetail qna_num={qna_num} />
+        <div className="grid col-span-4 auto-rows-max">
+          <h1 className=" pt-10 pb-5 text-xl font-bold">1:1 문의</h1>
+          <QnaDetail qna_num={qna_num} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="grid grid-cols-5">
+        <div>
+          <SideNav />
+        </div>
+
+        <div className="grid col-span-4 auto-rows-max">
+          <h1 className=" pt-10 pb-5 text-xl font-bold">1:1 문의</h1>
+          <QnaDetail qna_num={qna_num} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PageQnaDetail;
--- src/Pages/qna/PageQnaForm.jsx
+++ src/Pages/qna/PageQnaForm.jsx
@@ -1,29 +1,53 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import QnaForm from 'Components/qna/QnaForm';
 import SideNav from 'SideNav';
+import { useAuth } from 'Base/Context/AuthContext';
+import SideNavAdmin from 'SideNavAdmin';
 
 function PageQnaForm() {
   const navigate = useNavigate();
   const { qna_num } = useParams();
+  const [auth] = useAuth();
 
-  return (
-    <div className="grid grid-cols-5">
-      <div>
-        <SideNav />
-      </div>
+  if (auth.isLoggedIn && auth.is_superuser) {
+    return (
+      <div className="grid grid-cols-5">
+        <div>
+          <SideNavAdmin />
+        </div>
 
-      <div className="grid col-span-4 auto-rows-max">
-        <h1 className=" pt-8  text-xl font-bold">1:1 문의</h1>
-        <h6 className=" flex justify-end m-1 text-xs text-gray-400">
-          * 비방 및 욕설은 관리자에 의해 삭제될 수 있습니다.
-        </h6>
-        <QnaForm
-          qna_num={qna_num}
-          handleDidSave={(savedQna) => navigate(`/qna/${savedQna.qna_num}/`)}
-        />
+        <div className="grid col-span-4 auto-rows-max">
+          <h1 className=" pt-8  text-xl font-bold">1:1 문의</h1>
+          <h6 className=" flex justify-end m-1 text-xs text-gray-400">
+            * 비방 및 욕설은 관리자에 의해 삭제될 수 있습니다.
+          </h6>
+          <QnaForm
+            qna_num={qna_num}
+            handleDidSave={(savedQna) => navigate(`/qna/${savedQna.qna_num}/`)}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="grid grid-cols-5">
+        <div>
+          <SideNav />
+        </div>
+
+        <div className="grid col-span-4 auto-rows-max">
+          <h1 className=" pt-8  text-xl font-bold">1:1 문의</h1>
+          <h6 className=" flex justify-end m-1 text-xs text-gray-400">
+            * 비방 및 욕설은 관리자에 의해 삭제될 수 있습니다.
+          </h6>
+          <QnaForm
+            qna_num={qna_num}
+            handleDidSave={(savedQna) => navigate(`/qna/${savedQna.qna_num}/`)}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PageQnaForm;
