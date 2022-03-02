86f41ef049b86d0024152c8105062b6e968bb3c7
Yerin Park <1472moment@gmail.com>
2022-03-03 04:14:59 +0900

    PageClothesIndex 수정 // 빌려줄래요 버튼 구분

--- src/Pages/clothes/PageClothesIndex.jsx
+++ src/Pages/clothes/PageClothesIndex.jsx
@@ -1,17 +1,34 @@
+import { useAuth } from 'Base/Context/AuthContext';
 import ClothesList from 'Components/clothes/ClothesList';
+import PageLogin from 'Pages/accounts/PageLogin';
 import { useNavigate } from 'react-router-dom';
 
 function PageClothesIndex() {
   const navigate = useNavigate();
-  return (
-    <div>
-      <div className="float-right my-5 mr-10">
-        <button onClick={() => navigate('/clothes/new/')}>빌려줄래요!</button>
-      </div>
+  const [auth] = useAuth();
+  if (auth.isLoggedIn) {
+    return (
+      <div>
+        <div className="float-right my-5 mr-10">
+          <button onClick={() => navigate('/clothes/new/')}>빌려줄래요!</button>
+        </div>
 
-      <ClothesList />
-    </div>
-  );
+        <ClothesList />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div className="float-right my-5 mr-10">
+          <button onClick={() => navigate('/accounts/login/')}>
+            빌려줄래요!
+          </button>
+        </div>
+
+        <ClothesList />
+      </div>
+    );
+  }
 }
 
 export default PageClothesIndex;
