53f130606f88ee3b9a4ff9d9bc44131e7b270380
back hae ran <idg9833@gmail.com>
2022-03-02 18:03:41 +0900

    notice 구현 // noticeList css 구현

--- src/Components/notice/NoticeSummary.jsx
+++ src/Components/notice/NoticeSummary.jsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom';
 
-function NoticeSummary({ notice }) {
+function NoticeSummary({ notice, index }) {
   return (
-    <div className="flex">
-      <Link to={`/notice/${notice.notice_num}/`}>{notice.title}</Link>
-      <div className="ml-20">{notice?.user_id?.user_nickname}</div>
-    </div>
+    <>
+      <tbody className="border-b border-gray-150">
+        <tr>
+          <td className="p-4 pl-6">{index + 1}</td>
+          <td>
+            <Link to={`/notice/${notice.notice_num}/`}>{notice.title}</Link>
+          </td>
+          <td className="ml-20">{notice?.user_id?.user_nickname}</td>
+          <td className="text-right pr-5">
+            {notice.registration_date.slice(0, 10)}
+          </td>
+        </tr>
+      </tbody>
+    </>
   );
 }
 
