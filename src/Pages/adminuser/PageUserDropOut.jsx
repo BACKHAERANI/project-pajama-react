dc2d823b5975753350138cae90f295364bc177be
Yerin Park <1472moment@gmail.com>
2022-03-04 05:17:41 +0900

    탈퇴회원 grid 적용 // PageUserDropOut.jsx

--- src/Pages/adminuser/PageUserDropOut.jsx
+++ src/Pages/adminuser/PageUserDropOut.jsx
@@ -8,12 +8,13 @@ function PageUserDropOut() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex">
-      <SideNavAdmin />
+    <div className="grid grid-cols-5">
       <div>
-        <div className="flex pt-20 pr-100 text-lg">
-          <h1>탈퇴회원관리</h1>
-        </div>
+        <SideNavAdmin />
+      </div>
+
+      <div className="grid col-span-4 auto-rows-max">
+        <h1 className=" pt-7 text-xl font-bold">탈퇴회원관리</h1>
         <UserDropOut />
       </div>
     </div>
