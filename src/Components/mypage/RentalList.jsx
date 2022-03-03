acfe3287b1eb8eb79f5b8edadf76384b868cebf9
back hae ran <idg9833@gmail.com>
2022-03-03 16:26:20 +0900

    review 구현// 대역내역 css, 리뷰쓰면 버튼 보이지 않도록 구현

--- src/Components/mypage/RentalList.jsx
+++ src/Components/mypage/RentalList.jsx
@@ -1,21 +1,21 @@
 import { useApiAxios } from 'Base/api/base';
 import { useAuth } from 'Base/Context/AuthContext';
-import DebugStates from 'DebugStates';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import SideNav from 'SideNav';
 import ReactPaginate from 'react-paginate';
 
-function RentalList({ itemsPerPage = 10 }) {
+function RentalList() {
   const navigate = useNavigate();
   const [auth] = useAuth();
   const [currentItems, setCurrentItems] = useState(null);
   const [pageCount, setPageCount] = useState();
   const [query, setQuery] = useState();
   const [reload, setReload] = useState(false);
-  const [page, setPage] = useState(0);
   const [{ data: payment_detail, loading, error }, refetch] = useApiAxios(
-    { url: '/payment/api/payment_detail/', method: 'GET' },
+    {
+      url: '/payment/api/payment_detail/',
+      method: 'GET',
+    },
     { manual: true },
   );
 
@@ -26,7 +26,6 @@ function RentalList({ itemsPerPage = 10 }) {
   useEffect(() => {
     refetch()
       .then(({ data }) => {
-        setPageCount(Math.ceil((data?.count ? data.count : 1) / itemsPerPage));
         setCurrentItems(data?.results);
         setQuery('');
       })
@@ -35,60 +34,68 @@ function RentalList({ itemsPerPage = 10 }) {
       });
   }, [reload]);
 
-  const handlePageClick = (event) => {
-    setPage(event.selected);
-  };
-
   return (
-    <div className="flex">
-      <SideNav />
+    <>
+      <div className="grid grid-cols-8 mt-8">
+        <div className="col-span-8">
+          <h1 className="text-xl font-bold m-auto">대여내역</h1>
+        </div>
+        <hr className="mt-4 col-span-8 pt-5 border-t-2 border-gray-400" />
+        <div className="col-start-1 m-auto">번호</div>
+        <div className="col-start-2 m-auto">상품정보</div>
+        <div className="col-start-6 m-auto">가격</div>
+        <div className="col-start-7 col-span-2 m-auto">리뷰</div>
+      </div>
 
-      {payment_detail &&
-        payment_detail
-          ?.filter((List) => List.payment_num.user_id.user_id === auth.user_id)
-          .map((check) => {
-            return (
-              <div className="grid col-span-4 auto-rows-max">
-                <h1 className=" pt-8  text-xl font-bold">대여내역</h1>
-                <div
-                  className="grid grid-cols-5 grid-rows-5 border-t"
-                  key={check.payment_detail_num}
-                >
-                  <div className="row-span-1 col-span-1 border-gray-300">
-                    <img className="h-20 w-20" src={check.clothes_num.img1} />
-                  </div>
-                  <div className="row-span-1 col-span-1">
-                    {check.clothes_num.title}
-                  </div>
-                  <div className="row-span-2 col-span-2">
-                    {check.clothes_num.price}원
-                  </div>
-                  <div className=" flex justify-end p-1  text-sm align-middle">
-                    <button
-                      className="w-24 h-8 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600"
-                      onClick={() =>
-                        navigate(`/review/${check.payment_detail_num}/new/`)
-                      }
-                    >
-                      리뷰쓰기
-                    </button>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+      <div className="grid grid-cols-8">
+        <div className=""></div>
 
-      <ReactPaginate
-        className="pagination"
-        breakLabel="..."
-        previousLabel="<"
-        nextLabel=">"
-        pageCount={pageCount}
-        pageRangeDisplayed={itemsPerPage}
-        onPageChange={handlePageClick}
-        renderOnZeroPageCount={null}
-      />
-    </div>
+        <div className="col-span-8">
+          {payment_detail &&
+            payment_detail
+              ?.filter(
+                (List) => List.payment_num.user_id.user_id === auth.user_id,
+              )
+              .map((check, index) => {
+                return (
+                  <div className="grid grid-cols-8 my-3 pt-3 border-t border-gray-400">
+                    <div className="m-auto">{index + 1}</div>
+                    <>
+                      <div className="col-start-2 m-auto">
+                        <img
+                          className="w-24 h-24 object-cover shrink-0"
+                          src={check.clothes_num.img1}
+                        />
+                      </div>
+                      <div className="col-start-3 col-span-2 m-auto">
+                        <p>{check.clothes_num.title}</p>
+                      </div>
+
+                      <div className="col-start-6 m-auto">
+                        {check.clothes_num.price}원
+                      </div>
+                      {check.review.length === 0 && (
+                        <div className="col-start-7 col-span-2 m-auto">
+                          <button
+                            className="w-24 h-8 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600"
+                            onClick={() =>
+                              navigate(
+                                `/review/${check.payment_detail_num}/new/`,
+                              )
+                            }
+                          >
+                            리뷰쓰기
+                          </button>
+                        </div>
+                      )}
+                    </>
+                  </div>
+                );
+              })}
+          <hr className="mt-4 col-span-8 pt-5 border-t-2 border-gray-400" />
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/Pages/mypage/PagerentalList.jsx
+++ src/Pages/mypage/PagerentalList.jsx
@@ -1,9 +1,17 @@
 import RentalList from 'Components/mypage/RentalList';
+import SideNav from 'SideNav';
 
 function PagerentalList() {
   return (
     <div>
-      <RentalList />
+      <div className="grid grid-cols-5">
+        <div>
+          <SideNav />
+        </div>
+        <div className="col-span-4">
+          <RentalList />
+        </div>
+      </div>
     </div>
   );
 }
