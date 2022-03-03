0ebff9cea68170b2db6d68ab58d98960b6e29844
back hae ran <idg9833@gmail.com>
2022-03-03 18:31:49 +0900

    adminuser 구현 // 개인회원, 탈퇴회원 css 1차 구현

--- src/Components/adminuser/UserDropOut.jsx
+++ src/Components/adminuser/UserDropOut.jsx
@@ -5,11 +5,8 @@ import ReactPaginate from 'react-paginate';
 import 'Base/css/Pagination.css';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
-function UserDropOut({ user_id, itemsPerPage = 10 }) {
+function UserDropOut({ user_id }) {
   const [query, setQuery] = useState();
-  const [currentItems, setCurrentItems] = useState(null);
-  const [pageCount, setPageCount] = useState();
-  const [page, setPage] = useState(0);
   const [reload, setReload] = useState(false);
   const [checkedInputs, setCheckedInputs] = useState([]);
   const [auth] = useAuth();
@@ -17,9 +14,7 @@ function UserDropOut({ user_id, itemsPerPage = 10 }) {
 
   const [{ data: userList, loading, error }, getUser] = useApiAxios(
     {
-      url: `/user/api/users/?${page ? 'page=' + (page + 1) : 'page=1'}${
-        query ? '&query=' + query : ''
-      }`,
+      url: `/user/api/users/?all${query ? '&query=' + query : ''}`,
       method: 'GET',
     },
     { manual: true },
@@ -61,23 +56,9 @@ function UserDropOut({ user_id, itemsPerPage = 10 }) {
     setCheckedInputs([]);
   };
 
-  // const handleDelete = () => {
-  //   if (window.confirm('삭제하시겠습니까?')) {
-  //     deleteuser().then(() => {
-  //       navigate('/admin/');
-  //     });
-  //   }
-  // };
-
-  useEffect(() => {
-    setReload((prevState) => !prevState);
-  }, [page]);
-
   useEffect(() => {
     getUser()
       .then(({ data }) => {
-        setPageCount(Math.ceil((data?.count ? data.count : 1) / itemsPerPage));
-        setCurrentItems(data?.results);
         setQuery('');
       })
       .catch((error) => {
@@ -85,79 +66,81 @@ function UserDropOut({ user_id, itemsPerPage = 10 }) {
       });
   }, [reload]);
 
-  const handlePageClick = (event) => {
-    setPage(event.selected);
-  };
-
   const search = (e) => {
     if (e.key === 'Enter') {
       const { value } = e.target;
       setQuery(value);
-      setPage(0);
       setReload((prevState) => !prevState);
     }
   };
 
-  console.log(auth.user_id && auth.username && !auth.is_active);
-
   return (
     <div>
-      <div className="text-right mb-2">
+      {loading && '로딩 중 ...'}
+      {error && '로딩 중 에러가 발생했습니다.'}
+      <div className="text-right mb-2 flex justify-end">
         <input
-          className="w-80 h-10"
+          className="w-70 h-10 border-b border-gray-700 outline-none"
           type="search"
           name="search"
           placeholder="아이디로 검색할 수 있어요."
           onKeyPress={search}
         />
-      </div>
-      {loading && '로딩 중 ...'}
-      {error && '로딩 중 에러가 발생했습니다.'}
-      <div className="flex flex-row">
-        <h3 className="basis-1/5">번호</h3>
-        <h3 className="basis-1/5">아이디</h3>
-        <h3 className="basis-1/5">이름</h3>
-        <h3 className="basis-1/5">상태</h3>
-        <h3 className="basis-1/5">삭제버튼</h3>
+        <svg
+          fill="#000000"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 128 128"
+          className="w-7 h-7 mt-2"
+          width="128px"
+          height="128px"
+        >
+          <path d="M 52.349609 14.400391 C 42.624609 14.400391 32.9 18.1 25.5 25.5 C 10.7 40.3 10.7 64.399219 25.5 79.199219 C 32.9 86.599219 42.600391 90.300781 52.400391 90.300781 C 62.200391 90.300781 71.900781 86.599219 79.300781 79.199219 C 94.000781 64.399219 93.999219 40.3 79.199219 25.5 C 71.799219 18.1 62.074609 14.400391 52.349609 14.400391 z M 52.300781 20.300781 C 60.500781 20.300781 68.700391 23.399219 74.900391 29.699219 C 87.400391 42.199219 87.4 62.5 75 75 C 62.5 87.5 42.199219 87.5 29.699219 75 C 17.199219 62.5 17.199219 42.199219 29.699219 29.699219 C 35.899219 23.499219 44.100781 20.300781 52.300781 20.300781 z M 52.300781 26.300781 C 45.400781 26.300781 38.9 29 34 34 C 29.3 38.7 26.700391 44.800391 26.400391 51.400391 C 26.300391 53.100391 27.600781 54.4 29.300781 54.5 L 29.400391 54.5 C 31.000391 54.5 32.300391 53.199609 32.400391 51.599609 C 32.600391 46.499609 34.699219 41.799219 38.199219 38.199219 C 41.999219 34.399219 47.000781 32.300781 52.300781 32.300781 C 54.000781 32.300781 55.300781 31.000781 55.300781 29.300781 C 55.300781 27.600781 54.000781 26.300781 52.300781 26.300781 z M 35 64 A 3 3 0 0 0 32 67 A 3 3 0 0 0 35 70 A 3 3 0 0 0 38 67 A 3 3 0 0 0 35 64 z M 83.363281 80.5 C 82.600781 80.5 81.850781 80.800391 81.300781 81.400391 C 80.100781 82.600391 80.100781 84.499609 81.300781 85.599609 L 83.800781 88.099609 C 83.200781 89.299609 82.900391 90.6 82.900391 92 C 82.900391 94.4 83.8 96.700391 85.5 98.400391 L 98.300781 111 C 100.10078 112.8 102.39922 113.69922 104.69922 113.69922 C 106.99922 113.69922 109.29961 112.79961 111.09961 111.09961 C 114.59961 107.59961 114.59961 101.90039 111.09961 98.400391 L 98.300781 85.599609 C 96.600781 83.899609 94.300391 83 91.900391 83 C 90.500391 83 89.2 83.300391 88 83.900391 L 85.5 81.400391 C 84.9 80.800391 84.125781 80.5 83.363281 80.5 z M 91.900391 88.900391 C 92.700391 88.900391 93.5 89.200781 94 89.800781 L 106.69922 102.5 C 107.89922 103.7 107.89922 105.59922 106.69922 106.69922 C 105.49922 107.89922 103.6 107.89922 102.5 106.69922 L 89.800781 94.099609 C 89.200781 93.499609 88.900391 92.700391 88.900391 91.900391 C 88.900391 91.100391 89.200781 90.300781 89.800781 89.800781 C 90.400781 89.200781 91.100391 88.900391 91.900391 88.900391 z"></path>
+        </svg>
       </div>
 
-      {currentItems?.map(
-        (user, index) =>
-          !user.is_active && (
-            <div className="flex flex-row my-5" user={user} key={user.user_id}>
-              <div className="basis-1/5 text-lg"> {index + 1} </div>
-              <div className="basis-1/5">{user.user_id}</div>
-              <div className="basis-1/5">{user.username}</div>
-              <div className="basis-1/5">[비활성]</div>
-              <input
-                id={user.user_id}
-                type="checkbox"
-                onChange={(e) => {
-                  changeHandler(e.currentTarget.checked, user.user_id);
-                }}
-                checked={checkedInputs.includes(user.user_id) ? true : false}
-              />
-              <button
-                disabled={deleteLoading}
-                onClick={handleDelete}
-                className="basis-1/5 hover:text-red-400"
-              >
-                삭제
-              </button>
-            </div>
-          ),
-      )}
+      <table className="border-t-2  border-gray-150 w-full text-xs">
+        <thead className="border-b font-semibold border-gray-150">
+          <tr>
+            <td className="p-4 text-justify">번호</td>
+            <td className="p-4 text-center">아이디</td>
+            <td className="p-4 text-center">이름</td>
+            <td className="p-4 text-right">✔</td>
+          </tr>
+        </thead>
 
-      <ReactPaginate
-        className="pagination"
-        breakLabel="..."
-        previousLabel="<"
-        nextLabel=">"
-        pageCount={pageCount}
-        pageRangeDisplayed={itemsPerPage}
-        onPageChange={handlePageClick}
-        renderOnZeroPageCount={null}
-      />
+        {userList
+          ?.filter((user) => !user.is_active)
+          .map((user, index) => (
+            <tr>
+              <td className="p-4 pl-6">{index + 1}</td>
+              <td>
+                <Link className="p-4" to={`/admin/edit/${user.user_id}/`}>
+                  {user.user_id}
+                </Link>
+              </td>
+              <td className="p-4">{user.username}</td>
+              <td className="p-4">
+                <input
+                  className="mt-2"
+                  id={user.user_id}
+                  type="checkbox"
+                  onChange={(e) => {
+                    changeHandler(e.currentTarget.checked, user.user_id);
+                  }}
+                  checked={checkedInputs.includes(user.user_id) ? true : false}
+                />
+
+                <button
+                  className="ml-1 "
+                  disabled={deleteLoading}
+                  onClick={handleDelete}
+                >
+                  삭제
+                </button>
+              </td>
+            </tr>
+          ))}
+      </table>
     </div>
   );
 }
