cd855d43c38daf528930b8fee63f6de6234370eb
Yerin Park <1472moment@gmail.com>
2022-03-04 05:32:38 +0900

    로그인 폼 수정 // todo-remember me

--- src/Components/accounts/LoginForm.jsx
+++ src/Components/accounts/LoginForm.jsx
@@ -93,30 +93,11 @@ function LoginForm() {
                   name="password"
                   value={fieldValues.password}
                   placeholder="비밀번호를 입력해주세요"
-                  className="outline-none bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 "
+                  className="outline-none mb-3 bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 "
                   onChange={handleFieldChange}
                 />
               </div>
-              <div className="flex items-start">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      className="bg-gray-50 border border-gray-300 focus:ring-3 focus:ring-purple-300 h-4 w-4 rounded "
-                    />
-                  </div>
-                  <div class="text-sm ml-3">
-                    <label
-                      for="remember"
-                      className="font-medium text-gray-900 "
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                </div>
-              </div>
+
               <button
                 type="submit"
                 className="w-full text-white bg-purple-300 hover:bg-purple-300 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
