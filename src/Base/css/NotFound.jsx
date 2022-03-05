0219ae3c9acf3ac54187e072578b409b4b436ca0
Yerin Park <1472moment@gmail.com>
2022-03-06 02:54:41 +0900

    NotFound.jsx 수정 // 404 page 이미지 수정

--- src/Base/css/NotFound.jsx
+++ src/Base/css/NotFound.jsx
@@ -5,7 +5,7 @@ function NotFound() {
     <div>
       <div>
         <div className="flex justify-center">
-          <img className="w-5/6" src={pageNotfound} alt="404 페이지" />
+          <img className=" w-2/3" src={pageNotfound} alt="404 페이지" />
         </div>
       </div>
     </div>
