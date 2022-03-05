7e150367d30ef3ec008ef618aa5a03c0219da01b
back hae ran <idg9833@gmail.com>
2022-03-05 22:46:10 +0900

    review 구현 // ReviewSummary 닉네임, 등록일 보여주기

--- src/Components/review/ReviewSummary.jsx
+++ src/Components/review/ReviewSummary.jsx
@@ -3,9 +3,9 @@ import Rating from 'Pages/review/Rating';
 function ReviewSummary({ review }) {
   return (
     <div className="my-3">
-      <div className="">{review.nickname}</div>
       <Rating score={review.score} />
       <div>{review.title}</div>
+
       <div>{review.content}</div>
       <div>
         {review.img1 && (
@@ -43,6 +43,14 @@ function ReviewSummary({ review }) {
             className="w-20 h-20 rounded inline"
           />
         )}
+      </div>
+      <div className="flex ">
+        <div className="mr-2 text-gray-400">
+          {review?.payment_num.user_id.user_nickname}
+        </div>
+        <div className="mr-2 text-gray-400">
+          | {review.registration_date.slice(0, 10)}
+        </div>
       </div>
       <hr className="mt-3" />
     </div>
