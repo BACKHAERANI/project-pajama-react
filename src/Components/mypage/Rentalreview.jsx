37c7fac7eb3a1f1a8d8bc2e5a05711a30ac7d1d1
Yerin Park <1472moment@gmail.com>
2022-03-06 00:16:15 +0900

    Rentalreview.jsx CSS 수정 // 별점이랑 제목 위치 변경

--- src/Components/mypage/Rentalreview.jsx
+++ src/Components/mypage/Rentalreview.jsx
@@ -43,16 +43,15 @@ function Rentalreview({ payment_detail_num }) {
 
   return (
     <>
-      <div className="grid grid-cols-8 mt-8">
+      <div className="grid grid-cols-8 mt-8 font-semibold">
         <div className="col-span-8">
           <h1 className="text-xl font-bold m-auto">대여후기</h1>
         </div>
         <hr className="mt-4 col-span-8 pt-5 border-t-2 border-gray-400" />
         <div className="col-start-1 m-auto">번호</div>
-        <div className="col-start-2 m-auto">⭐</div>
-        <div className="col-start-3 m-auto">상품정보</div>
-        <div className="col-start-5 m-auto">리뷰</div>
-        <div className="col-start-8 m-auto">삭제</div>
+        <div className="col-start-2 m-auto">상품정보</div>
+        <div className="col-start-4 m-auto">⭐리뷰⭐</div>
+        <div className="col-start-8 ml-4">삭제</div>
       </div>
       <div className="grid grid-cols-8">
         <div className=""></div>
@@ -65,22 +64,21 @@ function Rentalreview({ payment_detail_num }) {
               <div className="grid grid-cols-8 my-3 pt-3 border-t border-gray-400">
                 <div className="m-auto">{index + 1}</div>
                 <>
-                  <Rating className="col-start-2 m-auto" score={review.score} />
-
-                  <div className="col-start-3 col-span-2 ">
+                  <div className="col-start-2 col-span-2 ">
                     <img
                       className="w-24 h-24 object-cover shrink-0"
                       src={review.clothes_num.img1}
                     />
-                    {review.clothes_num.title}
+                    <p className="font-semibold">{review.clothes_num.title}</p>
                   </div>
-                  <div className="col-start-5 ">
-                    <p className="font-bold">{review.title}</p>
+                  <div className="col-start-4 col-span-3 ml-3 my-auto">
+                    <Rating className=" " score={review.score} />
+                    <p className="mt-2 font-bold">{review.title}</p>
                     <p>{review.content}</p>
                   </div>
                   <div className="col-start-8">
                     <button
-                      className="mt-10 w-24 h-8 bg-gray-400 rounded-sm text-white transition duration-300 ease-in-out hover:bg-white hover:border hover:border-gray-400 hover:text-gray-600"
+                      className="mt-10 w-16 h-8 bg-white rounded-sm text-white border border-gray-400 text-gray-600"
                       onClick={handleDelete}
                       value={review.payment_detail_num}
                     >
@@ -92,6 +90,7 @@ function Rentalreview({ payment_detail_num }) {
             );
           })}
         </div>
+        <hr className="col-span-8 my-1 pt-1 border-t border-gray-400" />
       </div>
     </>
   );
