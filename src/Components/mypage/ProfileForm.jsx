4ca6d37f420df469870251dbf2b796b0d67a6d33
back hae ran <idg9833@gmail.com>
2022-03-07 02:42:55 +0900

    Profile 구현 // 개인정보수정 폼 css

--- src/Components/mypage/ProfileForm.jsx
+++ src/Components/mypage/ProfileForm.jsx
@@ -38,16 +38,17 @@ function ProfileForm() {
   return (
     <div>
       <div>
-        <div>
-          {DATA_FIELDS.map((dataType, index) => (
-            <div key={index} className="mb-6">
-              <input
-                type="text"
-                name={dataType}
-                onChange={handleFieldChange}
-                placeholder={dataType}
-                className="
-                      w-full
+        <div className="mt-16 flex justify-center">
+          <div className="w-2/5">
+            {DATA_FIELDS.map((dataType, index) => (
+              <div key={index} className="mb-6">
+                <input
+                  type="text"
+                  name={dataType}
+                  onChange={handleFieldChange}
+                  placeholder={dataType}
+                  className="
+                      w-80
                       rounded
                       py-3
                       px-[14px]
@@ -57,31 +58,37 @@ function ProfileForm() {
                       focus-visible:shadow-none
                       focus:border-primary
                       "
-              />
+                />
 
-              {errorMessages[dataType] &&
-                errorMessages[dataType].map((message, index) => (
-                  <p key={index} className="text-xs text-red-400">
-                    {message}
-                  </p>
-                ))}
+                {errorMessages[dataType] &&
+                  errorMessages[dataType].map((message, index) => (
+                    <p key={index} className="text-xs text-red-400">
+                      {message}
+                    </p>
+                  ))}
+              </div>
+            ))}
+
+            <div className="text-indigo-800 mb-6">
+              <h3>genre 선택 |</h3>
+              팝핀 브레이킹 락킹 왁킹 힙합 하우스 크럼프 기타
             </div>
-          ))}
 
-          <div className="flex justify-end">
-            <button
-              onClick={handleSubmit}
-              className="mr-3 text-white bg-purple-300 hover:bg-purple-300 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
-            >
-              수정
-            </button>
+            <div className="flex justify-end">
+              <button
+                onClick={handleSubmit}
+                className="mr-3 text-white bg-purple-300 hover:bg-purple-300 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+              >
+                수정
+              </button>
 
-            <button
-              onClick={() => navigate(`/profile/${auth.user_id}/`)}
-              className="text-white bg-purple-300 hover:bg-purple-300 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
-            >
-              취소
-            </button>
+              <button
+                onClick={() => navigate(`/profile/${auth.user_id}/`)}
+                className="text-white bg-purple-300 hover:bg-purple-300 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+              >
+                취소
+              </button>
+            </div>
           </div>
         </div>
       </div>
